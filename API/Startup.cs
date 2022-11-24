0e937641dfabec6b1e0f828819fd3f013fb58133
vijayh143 <vijayakumarax.hulmani@intel.com>
2022-11-23 17:51:13 -0800

    End of Section 3

--- API/Startup.cs
+++ API/Startup.cs
@@ -31,6 +31,11 @@ namespace API
                 
             });
             
+            services.AddCors(opt =>{
+                opt.AddPolicy("CorsPolicy", policy => {
+                    policy.AllowAnyMethod().AllowAnyHeader().WithOrigins("http://localhost:3000");
+                });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -46,6 +51,8 @@ namespace API
             //app.UseHttpsRedirection();
 
             app.UseRouting();
+
+            app.UseCors("CorsPolicy");
 
             app.UseAuthorization();
 
