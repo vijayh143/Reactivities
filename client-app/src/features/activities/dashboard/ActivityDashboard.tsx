b248fa7beff8cec3a9444cdc5a50e7a9c15bb41c
vijayh143 <VIJAYH143@GMAIL.COM>
2023-08-08 14:32:48 -0700

    End of Section 9

--- client-app/src/features/activities/dashboard/ActivityDashboard.tsx
+++ client-app/src/features/activities/dashboard/ActivityDashboard.tsx
@@ -1,9 +1,10 @@
-import React, { useEffect } from "react";
-import { Grid} from "semantic-ui-react";
+import { useEffect } from "react";
+import { Grid } from "semantic-ui-react";
 import ActivityList from "./ActivityList";
 import { useStore } from "../../../app/stores/store";
 import { observer } from "mobx-react-lite";
 import LoadingComponent from "../../../app/layout/LoadingComponent";
+import ActivityFilters from "./ActivityFilters";
 
 
 
@@ -23,7 +24,7 @@ export default observer(function ActivityDashboard( ) {
           <ActivityList />
         </Grid.Column>
         <Grid.Column width="6">
-           <h2>Activity Filters</h2>
+           <ActivityFilters/>
         </Grid.Column>
       </Grid>
     );
